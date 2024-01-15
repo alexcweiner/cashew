b1bbe162ba2cb4139f92d9041b92b68742c11450
Alex Weiner <alexcweiner@gmail.com>
2024-01-15 00:07:45 -0500

    token gen by curl

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,11 +1,9 @@
-curl -X POST http://localhost/api/register \
+curl -X POST http://localhost/registration/register \
 -H "Content-Type: application/json" \
 -d '{"username": "user1", "email": "user1@example.com", "password": "pass1", "roles": ["role1", "role2"]}'
 
-curl -X POST http://localhost/api/register \
--H "Content-Type: application/json" \
--d '{"username": "user2", "email": "user2@example.com", "password": "pass2", "roles": ["role1"]}'
 
-curl -X POST http://localhost/db/newuser \
+curl -X POST http://localhost/auth/token \
      -H "Content-Type: application/json" \
-     -d '{"username": "your_username", "email": "your_email", "password": "your_hashed_password"}'
+     -d '{"username": "user1", "password": "pass1"}' | jq -r '.token' | awk '{print "localStorage.setItem(\"token\", \""$1"\");"}'
+
